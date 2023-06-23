e04e030b97458beabbead07fe620ccfd5a733bf9
NovichenkoEvgeniy <tnrkip@yandex.ru>
2023-06-23 22:03:03 +0300

    seminar 1

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+// 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+//        System.out.print("Введите число n ");
+//        Scanner scanner = new Scanner(System.in);
+//        int summa=0;
+//        int factorial=1;
+//        int n = scanner.nextInt();
+//        for (int i = 1; i <= n;summa+=i,factorial*=i, i++);
+//        System.out.print(summa+" "+factorial);
+
+//        2) Вывести все простые числа от 1 до 1000
+//        for (int i = 1; i <= 1000; i++) {
+//            boolean isPrimeNumber = true;
+//            for (int j = 2; j < i; j++) {
+//                if (i % j == 0) {
+//                    isPrimeNumber = false;
+//                    break;
+//                }
+//            }
+//            if (isPrimeNumber) {
+//                System.out.print(i + " ");
+//            }
+//        }
+
+//        3) Реализовать простой калькулятор
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите число 1 ");
+//        int n1 = scanner.nextInt();
+//        System.out.println("Введите знак ");
+//        String s  = scanner.next();
+//        System.out.print("Введите число 2 ");
+//        int n2 = scanner.nextInt();
+//
+//        switch (s){
+//            case "+" :
+//                System.out.println(n1+n2);
+//                break;
+//            case "-":
+//                System.out.println(n1-n2);
+//                break;
+//            case "*":
+//                System.out.println(n1*n2);
+//                break;
+//            case "/":
+//                System.out.println(n1/n2);
+//                break;
+//            default:
+//                System.out.println("Что то пошло не так");
+//                break;
+
+    }
+
+}
+
+

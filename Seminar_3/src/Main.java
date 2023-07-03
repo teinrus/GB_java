a5c605f22f3c5705e0103b244425cfa1af81c0f3
NovichenkoEvgeniy <tnrkip@yandex.ru>
2023-07-03 23:53:38 +0300

    seminar 3

--- Seminar_3/src/Main.java
+++ Seminar_3/src/Main.java
@@ -1,8 +1,5 @@
 import java.util.*;
-import java.util.stream.IntStream;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
